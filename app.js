f7bcc87312ccbfb06f51b2bee0631e7653277c28
Vincent Rohde <vincent.rohde@icloud.com>
2018-06-02 10:58:44 +0200

    Remove prefix

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const fs = require('fs');
-let folder = process.argv[2];
+const folder = process.argv[2];
+const src = __dirname + '/' + folder;
 
 // now only works with subdirectories
-fs.readdir(__dirname + '/' + folder, function (err, items) {
-  console.log(items);
+fs.readdir(src, (err, items) => {
+
+  items.forEach((item) => {
+    const original = item;
+    let newFilename = '';
+
+    // remove/ safe extension name from filename
+    let parts = item.split('.');
+    const extension = parts[parts.length - 1];
+    parts.pop();
+
+    parts.forEach((item) => {
+      let parts = item.split('_');
+      // remove prefix
+      parts.shift();
+      // put file parts back together
+      newFilename = parts.join('_');
+    });
+
+    newFilename += '.' + extension;
+    // renames file to new name
+    fs.rename(src + '/' + original, src + '/' + newFilename, (err) => {
+      if(err) {
+        console.log(err);
+      }
+    });
+  });
 });
