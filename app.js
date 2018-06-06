acfe3932620e6d6307a126ce5a40027a6af14652
Vincent Rohde <vincent.rohde@icloud.com>
2018-06-06 18:34:18 +0200

    Add prefix/suffix feature

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 const fs = require('fs');
+const fixfix = require('./fixfix');
+
 const folder = process.argv[2];
+const division = process.argv[3];
+const fix = process.argv[4];
 const src = __dirname + '/' + folder;
 
-// now only works with subdirectories
-fs.readdir(src, (err, items) => {
-
-  items.forEach((item) => {
-    const original = item;
-    let newFilename = '';
-
-    // remove/ safe extension name from filename
-    let parts = item.split('.');
-    const extension = parts[parts.length - 1];
-    parts.pop();
-
-    parts.forEach((item) => {
-      let parts = item.split('_');
-      // remove prefix
-      parts.shift();
-      // put file parts back together
-      newFilename = parts.join('_');
-    });
-
-    newFilename += '.' + extension;
-    // renames file to new name
-    fs.rename(src + '/' + original, src + '/' + newFilename, (err) => {
-      if(err) {
-        console.log(err);
-      }
-    });
-  });
-});
+// folder has to be in a sub directory
+fixfix.fix(src, division, fix);
